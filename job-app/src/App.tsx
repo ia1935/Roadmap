0e54ed0a146e07936d49413d8996c1bbc0a46b94
Imran Ahmed <imranhahmed0987@gmail.com>
2025-02-23 23:58:07 -0500

    frontend init commit

---
+++ job-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
