2d3f23911c1a9366f2b7684c455e6b640c461a78
Imran Ahmed <imranhahmed0987@gmail.com>
2025-03-07 12:34:14 -0500

    Update settings.py

--- server/job_tracker_app/job_tracker_app/settings.py
+++ server/job_tracker_app/job_tracker_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)!qzxlcs1l0vrq*w8u1p%xka-j5vg3l7ed&5pe+zyl!ik-1llj'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
